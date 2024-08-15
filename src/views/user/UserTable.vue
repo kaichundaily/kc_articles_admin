ceb9dbefde62073e0f9f217b32e7ee742f890f0c
kc <kaichunwork@163.com>
2024-08-15 16:47:17 +0800

    半成品-创建用户

--- src/views/user/UserTable.vue
+++ src/views/user/UserTable.vue
@@ -3,6 +3,8 @@ import { ref } from "vue";
 import { getAllUser } from '@/api/user.js'
 import { message } from "ant-design-vue";
 
+
+// table`s header
 const columns = ref([
   {
     title: 'ID',
@@ -32,7 +34,7 @@ const columns = ref([
 ])
 
 
-
+ // 表格数据 和 分页功能
 const pagination = ref({
   current: 1,
   pageSize: 10,
@@ -49,7 +51,6 @@ const data = ref([])
 const loading = ref(false)
 const getAllUserInfo = async (page, size) => {
   loading.value = true
-  console.log(typeof page)
   let res = null
   try {
     res = await getAllUser(page, size)
@@ -63,13 +64,20 @@ const getAllUserInfo = async (page, size) => {
   pagination.value.total = res.data.total
   loading.value = false
 }
-getAllUserInfo(1, 10)
+getAllUserInfo(1, 10)  // 初始化数据, 默认一页10条
+
+// 添加用户相关逻辑
+const showDrawer = ref(false)
+const drawerSubmit = () => {
+  console.log("已提交")
+  showDrawer.value = false
+}
 </script>
 
 <template>
   <div>
     <div>
-      <a-button type="primary">添加用户</a-button>
+      <a-button type="primary" @click="showDrawer = true">添加用户</a-button>
     </div>
     <a-table
         :columns="columns"
@@ -94,6 +102,59 @@ getAllUserInfo(1, 10)
         </template>
       </template>
     </a-table>
+
+    <a-drawer :width="500" title="添加用户" placement="right" :open="showDrawer" @close="showDrawer = false">
+      <template #extra>
+        <a-button style="margin-right: 8px" @click="showDrawer = false">取消</a-button>
+        <a-button type="primary" @click="drawerSubmit">提交</a-button>
+      </template>
+      <a-form>
+        <a-form-item>
+          <div>头像:</div>
+          <a-upload
+              v-model:file-list="fileList"
+              name="avatar"
+              list-type="picture-card"
+              class="avatar-uploader"
+              :show-upload-list="false"
+              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+              :before-upload="beforeUpload"
+              @change="handleChange"
+          >
+            <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
+            <div v-else>
+              <loading-outlined v-if="loading"></loading-outlined>
+              <plus-outlined v-else></plus-outlined>
+              <div class="ant-upload-text">Upload</div>
+            </div>
+          </a-upload>
+        </a-form-item>
+        <a-form-item>
+          <div>账户:</div>
+          <a-input placeholder="请输入账户">
+            <template #prefix>
+              <UserOutlined class="site-form-item-icon" />
+            </template>
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <div>密码:</div>
+          <a-input-password placeholder="请输入密码">
+            <template #prefix>
+              <LockOutlined class="site-form-item-icon" />
+            </template>
+          </a-input-password>
+        </a-form-item>
+        <a-form-item>
+          <div>密码验证:</div>
+          <a-input-password placeholder="请再次输入密码">
+            <template #prefix>
+              <LockOutlined class="site-form-item-icon" />
+            </template>
+          </a-input-password>
+        </a-form-item>
+      </a-form>
+    </a-drawer>
   </div>
 </template>
 
