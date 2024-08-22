97518b5779deff99736050e0214fb233635bd22e
kc <kaichunwork@163.com>
2024-08-22 19:06:07 +0800

    测试文章

--- src/views/article/ArticleTable.vue
+++ src/views/article/ArticleTable.vue
@@ -6,43 +6,79 @@ import {
     CheckOutlined,
     CloseOutlined
 } from '@ant-design/icons-vue'
-import EditArticle from "@/views/article/modules/EditArticle.vue";
+import { message } from "ant-design-vue";
+import { MdEditor } from "md-editor-v3";
+import { testArticle } from "@/views/article/js/testArticle.js";
 const loading = ref(false)
 const columns = articleTableColumns()
 
-// const showEditArticleDrawer = ref(false)
+const testContent = testArticle()
 
 const data = ref([{
   title: "测试文章",
   tag: "测试文章",
   status: true,
   public: false,
-  content: "# 测试文章\n\n## 测试第一节\n\n"
+  content: testContent
 }])
-const dataSet = ref({
-  nowContent: "",
-  nowTitle: "",
-  id: '',
-  showEditArticleDrawer: false
-})
 
-const openShowEditArticle = (newContent, newTitle, id) => {
-  dataSet.value.nowContent = newContent
-  dataSet.value.nowTitle = newTitle
-  dataSet.value.id = id
-  dataSet.value.showEditArticleDrawer = true
+const content = ref("")
+const title = ref("")
+const id = ref("")
+const showMdEditor = ref(false)
+
+const openShowEditArticle = (newContent, newTitle, newID) => {
+  content.value = newContent
+  title.value = newTitle
+  id.value = newID
+  showMdEditor.value = true
 }
 
 const closeShowEditArticle = () => {
-  dataSet.value.nowContent = ""
-  dataSet.value.nowTitle = ""
-  dataSet.value.id = ""
-  dataSet.value.showEditArticleDrawer = false
+  content.value = ""
+  title.value = ""
+  id.value = ""
+  showMdEditor.value = false
 }
 
-// onBeforeMount(() => {
-//   childRef.value.toContent()
-// })
+const submit = () => {
+  message.success("模拟提交成功")
+  closeShowEditArticle()
+}
+
+const customToolbar = ref([
+  'bold',
+  'underline',
+  'italic',
+  '-',
+  'title',
+  'strikeThrough',
+  'sub',
+  'sup',
+  'quote',
+  'unorderedList',
+  'orderedList',
+  'task',
+  '-',
+  'codeRow',
+  'code',
+  'link',
+  'image',
+  'table',
+  'mermaid',
+  'katex',
+  '-',
+  'revoke',
+  'next',
+  'save',
+  '=',
+  'pageFullscreen',
+  'fullscreen',
+  'preview',
+  'previewOnly',
+  'htmlPreview',
+  'catalog',
+])
 </script>
 
 <template>
@@ -85,11 +121,28 @@ const closeShowEditArticle = () => {
         <EditOutlined class="custom-float-button-icon"/>
       </template>
     </a-float-button>
-    <edit-article
-        ref="childRef"
-        :dataSet="dataSet"
-        @closeShowEditArticle="closeShowEditArticle"
-    />
+    <!--  文章编辑功能  -->
+    <a-modal
+      :title="title || '编辑内容'"
+      :width="`60%`"
+      :open="showMdEditor"
+      @close="closeShowEditArticle"
+      cancel-text="取消编辑"
+      ok-text="提交"
+      @cancel="closeShowEditArticle"
+      @ok="submit"
+    >
+      <div class="custom-modal" style="height: 800px">
+        <MdEditor
+          theme="dark"
+          preview-theme="cyanosis"
+          v-model="content"
+          :preview="true"
+          style="height: 100%"
+          :toolbars="customToolbar"
+        />
+      </div>
+    </a-modal>
   </div>
 </template>
 
@@ -100,5 +153,8 @@ const closeShowEditArticle = () => {
   .custom-float-button-icon{
     font-size: 20px;
   }
+}
+.custom-modal {
+  overflow: auto;
 }
 </style>
