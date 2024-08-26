e407bfe6bcda6287754529df3c529db94caded77
kc <kaichunwork@163.com>
2024-08-26 15:20:35 +0800

    小变动

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -16,7 +16,7 @@ onMounted(() => {
       xAxis: {
         type: 'category',
         boundaryGap: false,
-        data: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']
+        data: ['8-19', '8-20', '8-21', '8-22', '8-23', '8-24', '8-25']
       },
       yAxis: {
         type: 'value'
@@ -55,21 +55,21 @@ onMounted(() => {
         {
           name: 'Nightingale Chart',
           type: 'pie',
-          radius: [50, 250],
+          radius: ['20%', '70%'],
           center: ['50%', '50%'],
           roseType: 'area',
           itemStyle: {
             borderRadius: 8
           },
           data: [
-            { value: 40, name: 'rose 1' },
-            { value: 38, name: 'rose 2' },
-            { value: 32, name: 'rose 3' },
-            { value: 30, name: 'rose 4' },
-            { value: 28, name: 'rose 5' },
-            { value: 26, name: 'rose 6' },
-            { value: 22, name: 'rose 7' },
-            { value: 18, name: 'rose 8' }
+            { value: 40, name: 'vue' },
+            { value: 38, name: 'Go' },
+            { value: 32, name: 'react' },
+            { value: 30, name: 'linux' },
+            { value: 28, name: 'java' },
+            { value: 26, name: 'python' },
+            { value: 22, name: 'TS' },
+            { value: 18, name: 'panel' }
           ]
         }
       ]
@@ -84,16 +84,23 @@ onMounted(() => {
 </script>
 
 <template>
-  <a-row :gutter="16">
-    <a-col :span="16">
-      <div ref="zhexiantu" style="width: 100%; height: 400px; background-color: #fff"></div>
-    </a-col>
-    <a-col :span="8">
-      <div ref="bingtu" style="width: 100%; height: 600px; background-color: #fff"></div>
-    </a-col>
-  </a-row>
+  <div class="index">
+    <a-row :gutter="16">
+      <a-col :span="16">
+        <div ref="zhexiantu" style="position: relative; width: 100%; height: 30vh; background-color: #fff"></div>
+      </a-col>
+      <a-col :span="8">
+        <div ref="bingtu" style="position: relative; width: 100%; height: 30vh; background-color: #fff"></div>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <style scoped>
-
+.index {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-color: aquamarine;
+}
 </style>
